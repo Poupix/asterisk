e35817ca1afa521f3bd0b243296f4cbd504c1f38
Poupix <118933166+Poupix@users.noreply.github.com>
2024-04-19 22:28:18 +0200

    Update asterisk.sh

--- asterisk.sh
+++ asterisk.sh
@@ -3,7 +3,7 @@
 # Asterisk installation script
 #
 # Author: [Poupix83]
-# Version: 1.0.0
+# Version: 1.0.3
 
 warn() {
     echo -e '\e[31m'$1'\e[0m'
@@ -16,7 +16,7 @@ info() {
 # Function to check system privileges
 function check_root() {
     if [[ "$(id -u)" -ne 0 ]]; then
-        warn "Ce script doit être exécuté en tant que root" >&2
+        warn "Ce script doit être exécuté en tant que root" 
         exit 1
     else
         info "Privilège Root: OK"
@@ -30,7 +30,7 @@ check_package_manager() {
     elif command -v dnf &>/dev/null; then
         PACKAGE_MANAGER="dnf"
     else
-        warn "Neither apt nor dnf found. This script supports only apt and dnf package managers."
+        warn "Neither apt or dnf found. This script supports only apt and dnf package managers."
         exit 1
     fi
 }
@@ -39,37 +39,32 @@ check_package_manager() {
 update_packages() {
     info "Updating package list..."
     if [ "$PACKAGE_MANAGER" == "apt" ]; then
-        apt update
+        apt update &>/dev/null;
     elif [ "$PACKAGE_MANAGER" == "dnf" ]; then
-        dnf makecache
+        dnf makecache &>/dev/null;
     fi
 }
 
 # Upgrade installed packages
 upgrade_packages() {
     info "Upgrading installed packages..."
-    if [ "$PACKAGE_MANAGER" == "apt" ]; then
-        apt upgrade -y
-    elif [ "$PACKAGE_MANAGER" == "dnf" ]; then
-        dnf upgrade -y
-    fi
+           $PACKAGE_MANAGER upgrade -y &>/dev/null;
 }
 
 # SELinux in the permissive mode for RHED
 SELinux-permissive_mode() {
     info "Installing required packages..."
-    if ["$PACKAGE_MANAGER" == "dnf" ]; then
-        setenforce 0
-        sed -i 's/\(^SELINUX=\).*/\SELINUX=permissive/' /etc/selinux/config
+    if [ "$PACKAGE_MANAGER" == "dnf" ]; then
+        setenforce 0 &>/dev/null;
     fi
 }
 
 # Install required system packages for RHED
 install_system_packages() {
     info "Installing required packages..."
-    if ["$PACKAGE_MANAGER" == "dnf" ]; then
-        dnf -y install epel-release
-        dnf config-manager --enable crb
+    if [ "$PACKAGE_MANAGER" == "dnf" ]; then
+        dnf -y install epel-release &>/dev/null;
+        dnf config-manager --enable crb &>/dev/null;
     fi
 }
 
@@ -77,11 +72,11 @@ install_system_packages() {
 install_packages() {
     info "Installing required packages..."
     if [ "$PACKAGE_MANAGER" == "apt" ]; then
-        apt install -y curl initscripts
+        $PACKAGE_MANAGER install -y curl initscripts &>/dev/null;
     elif [ "$PACKAGE_MANAGER" == "dnf" ]; then
-        dnf group install -y "Development Tools"
-        dnf install -y curl chkconfig initscripts git net-tools sqlite-devel psmisc ncurses-devel newt-devel libxml2-devel libtiff-devel
-        dnf install -y gtk2-devel libtool libuuid-devel subversion kernel-devel kernel-devel-$(uname -r) crontabs cronie-anacron libedit libedit-devel
+       $PACKAGE_MANAGER group install -y "Development Tools" &>/dev/null;
+        $PACKAGE_MANAGER install -y curl wget chkconfig initscripts git net-tools sqlite-devel psmisc ncurses-devel newt-devel libxml2-devel libtiff-devel &>/dev/null;
+        $PACKAGE_MANAGER install -y gtk2-devel libtool libuuid-devel subversion kernel-devel kernel-devel-$(uname -r) crontabs cronie-anacron libedit libedit-devel &>/dev/null;
     fi
 }
 
@@ -89,17 +84,17 @@ install_packages() {
 install_jansson() {
     info "Download and install Jansson..."
     if [ "$PACKAGE_MANAGER" == "dnf" ]; then
-        git clone https://github.com/akheron/jansson.git
+        git clone https://github.com/akheron/jansson.git &>/dev/null;
     fi
     cd jansson/
     # Command to create build source code
-    autoreconf -i
+    autoreconf -i &>/dev/null;
     #Compile the package
-    ./configure --prefix=/usr/
+    ./configure --prefix=/usr/ &>/dev/null;
     #Maintain groups and files
-    make
+    make &>/dev/null;
     #Install the package
-    make install
+    make install &>/dev/null;
 }
 
 # Download and Install PJSIP
@@ -107,59 +102,47 @@ install_PJSIP() {
     cd ~/
     info "Download and install PJSIP..."
     if [ "$PACKAGE_MANAGER" == "dnf" ]; then
-        git clone https://github.com/pjsip/pjproject.git
+        git clone https://github.com/pjsip/pjproject.git &>/dev/null;
     fi
     cd pjproject/
     #Compile the package
-    ./configure CFLAGS="-DNDEBUG -DPJ_HAS_IPV6=1" --prefix=/usr --libdir=/usr/lib64 --enable-shared --disable-video --disable-sound --disable-opencore-amr
+    ./configure CFLAGS="-DNDEBUG -DPJ_HAS_IPV6=1" --prefix=/usr --libdir=/usr/lib64 --enable-shared --disable-video --disable-sound --disable-opencore-amr &>/dev/null;
     #Maintain groups and files
-    make dep
-    make
+    make dep &>/dev/null;
+    make &>/dev/null;
     #Install the package
-    make install
-    ldconfig
+    make install &>/dev/null;
+    ldconfig &>/dev/null;
 }
 
 # Test if apparmor active
 function is_started() {
-    service_name="$1"
-    service_status=$(systemctl status "$service_name" | grep "Active: " | awk '{print $2}')
-    if [[ $service_status == "active" ]]; then
-        info "$service_name started"
-        info "apparmor stopped"
-        systemctl stop apparmor
+   if [ -d "/etc/apparmor" ]; then
+        info "AppArmor est deja installer." 
+        apt -y remove apparmor &>/dev/null;
+        rm -rf /etc/apparmor*
     else
-        warn "$service_name not started"
-    fi
-}
-
-# Remove AppArmor package
-remove_apparmor() {
-    warn "Removing AppArmor package..."
-    if [ "$PACKAGE_MANAGER" == "apt" ]; then
-        apt remove -y apparmor
-    elif [ "$PACKAGE_MANAGER" == "dnf" ]; then
-        dnf remove -y apparmor
+        info "AppArmor n'est pas installer."
     fi
 }
 
 # Download the Asterisk source code
 download_asterisk() {
     info "Downloading Asterisk source code..."
-    wget -O /tmp http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz
+    wget -O /tmp/asterisk-20.tar.gz http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz &>/dev/null;
 }
 
-# Extract the downloaded file
+# Extract the downloaded file 
 extract_asterisk() {
     info "Extracting Asterisk source code..."
-    tar zxvf /tmp/asterisk-20*.tar.gz -C /usr/src/
-    mv asterisk-20* asterisk 
+    tar zxvf /tmp/asterisk-20.tar.gz -C /usr/src/ &>/dev/null;
+    mv /usr/src/asterisk-20* /usr/src/asterisk &>/dev/null;
 }
 
 # Remove the downloaded tar.gz file
 remove_tarball() {
     info "Removing downloaded tar.gz file..."
-    rm -rf /tmp/asterisk-20*.tar.gz
+    rm -rf /tmp/asterisk-20*.tar.gz &>/dev/null;
 }
 
 # Navigate into the extracted Asterisk directory
@@ -171,45 +154,46 @@ navigate_to_asterisk_dir() {
 # Install prerequisites
 install_prerequisites() {
     info "Installing Asterisk prerequisites..."
-    contrib/scripts/install_prereq install
+    contrib/scripts/install_prereq install &>/dev/null;
 }
 
 # Configure Asterisk
 configure_asterisk() {
     info "Configuring Asterisk..."
-    ./configure --with-jansson-bundled
+    ./configure --with-jansson-bundled &>/dev/null;
 }
 compile_asterisk() {
     info "Compiling Asterisk..."
-    ./menuselect/menuselect --enable app_dial --enable app_playback --enable codec_opus --enable format_wav menuselect.makeopts
+    make menuselect.makeopts &>/dev/null;
+    ./menuselect/menuselect --enable app_dial --enable app_playback --enable codec_opus --enable format_wav menuselect.makeopts &>/dev/null;
 }
 # Install Asterisk
 install_asterisk() {
     info "Installing Asterisk..."
-    make instal
+    make install &>/dev/null;
 }
 
 # Install sample configuration files
 install_sample_configs() {
     info "Installing sample configuration files..."
-    make samples
-    mkdir /etc/asterisk/samples
-    mv /etc/asterisk/*.* /etc/asterisk/samples/
+    make samples &>/dev/null;
+    mkdir /etc/asterisk/samples &>/dev/null;
+    mv -rf /etc/asterisk/*.* /etc/asterisk/samples/ &>/dev/null;
 }
 
 #Make basic-pbx lets you quickly run Asterisk as a simple PBX for testing and experimentation purposes.
 install_Make_basic_pbx() {
     echo "Make basic-pbx"
-    make basic-pbx
+    make basic-pbx &>/dev/null;
 }
 
 # Perform the final configuration
 perform_final_config() {
     info "Performing the final configuration..."
-    make config
+    make config &>/dev/null;
 }
 
-# Enable Asterisk to start at boot
+# Enable Asterisk to start at boot 
 enable_asterisk_service() {
     info "Enabling Asterisk to start at boot..."
     systemctl enable asterisk.service
@@ -230,7 +214,7 @@ check_asterisk_service_status() {
 # Access the Asterisk CLI
 access_asterisk_cli() {
     info "Accessing the Asterisk CLI..."
-    asterisk -rvvvv
+    asterisk -rvvv <<<$'exit\n'
 }
 
 # Configuring Asterisk with PJSIP
@@ -298,8 +282,8 @@ EOL
 # Clean up
 clean() {
     echo "Clean up..."
-    make clean
-    make distclean
+    make clean &>/dev/null;
+    make distclean &>/dev/null;
     echo "Asterisk has been successfully installed and configured."
 }
 
@@ -309,34 +293,47 @@ start_asterisk() {
     asterisk -rvvvvvvvvvvvvvvvvvvvv <<<$'core reload\ndialplan reload\nexit\n'
 }
 
-clear
-check_root
-check_package_manager
-update_packages
-upgrade_packages
-SELinux-permissive_mode
-install_system_packages
-install_packages
-install_jansson
-install_PJSIP
-is_started apparmor
-remove_apparmor
-download_asterisk
-extract_asterisk
-remove_tarball
-navigate_to_asterisk_dir
-install_prerequisites
-configure_asterisk
-compile_asterisk
-install_asterisk
-install_sample_configs
-install_Make_basic_pbx
-perform_final_config
-enable_asterisk_service
-start_asterisk_service
-check_asterisk_service_status
-access_asterisk_cli
-add_content_pjsip_conf
-add_content_extensions_conf
-clean
-start_asterisk
+    clear 
+    check_root
+    check_package_manager
+    update_packages
+    upgrade_packages
+    info "Time to update and upgrade packages:$SECONDS"
+    SELinux-permissive_mode
+    install_system_packages
+    info "Time to install system packages:$SECONDS"
+    install_packages
+    info "Time to install packages:$SECONDS"
+    install_jansson
+    info "Time to install Jansson:$SECONDS"
+    install_PJSIP
+    info "Time to install PJSIP:$SECONDS"
+    is_started apparmor
+    download_asterisk
+    extract_asterisk
+    remove_tarball
+    navigate_to_asterisk_dir
+    install_prerequisites
+    info "Time to install prerequisites:$SECONDS"
+    configure_asterisk
+    info "Time to configure Asterisk:$SECONDS"
+    compile_asterisk
+    info "Time to compile Asterisk:$SECONDS"
+    install_asterisk
+    info "Time to install Asterisk:$SECONDS"
+    install_sample_configs
+    info "Time to install sample configs:$SECONDS"
+    install_Make_basic_pbx
+    info "Time to install Make basic-pbx:$SECONDS"
+    perform_final_config
+    info "Time to perform final config:$SECONDS"
+    enable_asterisk_service
+    start_asterisk_service
+    check_asterisk_service_status
+    access_asterisk_cli
+    add_content_pjsip_conf
+    add_content_extensions_conf
+    clean
+    info "Time to clean:$SECONDS"
+    start_asterisk
+    info "Final time:$SECONDS"
